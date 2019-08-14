76c2561e11e5c1b9536d4d4b165923cbf7ed32f4
Chris Crutchfield <clcrutch@cox.net>
2019-08-13 17:54:41 -0700

    full setup Music table

--- orm/Music.py
+++ orm/Music.py
@@ -1,9 +1,17 @@
 from orm.Base import Base
 
-from sqlalchemy import Column, Integer, NVARCHAR
+from sqlalchemy import Column, Integer, NVARCHAR, FLOAT, VARBINARY
 
 class Music(Base):
     __tablename__ = "Music"
 
     id = Column(Integer, primary_key=True, name='ID')
     file_path = Column(NVARCHAR, name='FilePath')
+    length = Column(FLOAT, name='Length')
+    fingerprint = Column(VARBINARY, name='Fingerprint')
+    music_brainz_id = Column(NVARCHAR(256), name='MusicBrainzID')
+    title = Column(NVARCHAR(256), name='Title')
+    album_id = Column(Integer, name='AlbumID')
+    track = Column(Integer, name='Track')
+    genre = Column(NVARCHAR(256), name='Genre')
+    lyrics = Column(NVARCHAR, name='Lyrics')
